2f271a6086137df45c58612ef5d91216a8e35166
Maxime Pires <maximepires4@gmail.com>
2023-01-07 19:29:30 +0100

    feat: Added pathology cards

--- pages/api/search/[slug].js
+++
@@ -1,7 +0,0 @@
-import {db} from '../search'
-
-export default function handler(req, res) {
-    const pathology = db.filter( pathology => pathology.name.toLowerCase().includes(req.query.slug.toLowerCase()))
-    if( !pathology ) return res.status(404).json(null)
-    res.status(200).json(pathology)
-}
